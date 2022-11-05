a550fbbb3334943a6ca64cd3c3b0e686e2e9646e
Aurélien Leblond <blablack@gmail.com>
2022-11-05 13:23:06 +0000

    Fix path

--- docker-builder/script.sh
+++ docker-builder/script.sh
@@ -12,28 +12,28 @@ mkdir -p /data/podman
 
 (
     cd /data/fawnarevagh-kube/docker-builder 
-    podman --root /data/podman build -t docker-builder . 
+    podman build -t docker-builder . 
     podman tag docker-builder:latest nucio.nowhere:30038/docker-builder:latest
     podman push nucio.nowhere:30038/docker-builder:latest
 )
 
 (
     cd /data/fawnarevagh-kube/kublicity 
-    podman --root /data/podman build -t kublicity . 
+    podman build -t kublicity . 
     podman tag kublicity:latest nucio.nowhere:30038/kublicity:latest
     podman push nucio.nowhere:30038/kublicity:latest
 )
 
 (
     cd /data/fawnarevagh-kube/picsync 
-    podman --root /data/podman build -t picsync . 
+    podman build -t picsync . 
     podman tag picsync:latest nucio.nowhere:30038/picsync:latest
     podman push nucio.nowhere:30038/picsync:latest
 )
 
 (
-    cd /data/nordvpn/nordvpn
-    podman --root /data/podman build -t nordvpn . 
+    cd /data/fawnarevagh-kube/nordvpn
+    podman build -t nordvpn . 
     podman tag nordvpn:latest nucio.nowhere:30038/nordvpn:latest
     podman push nucio.nowhere:30038/nordvpn:latest
 )
