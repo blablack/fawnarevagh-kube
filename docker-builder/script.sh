0630e58ab06ad6ad49fc8698cc7d4295234b1d47
Aurélien Leblond <blablack@gmail.com>
2022-11-05 12:55:01 +0000

    use data folder

--- docker-builder/script.sh
+++ docker-builder/script.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # clone or pull repo
-if [ -d "fawnarevagh-kube" ] 
+if [ -d "/data/fawnarevagh-kube" ] 
 then
-    (cd fawnarevagh-kube ; git pull)
+    (cd /data/fawnarevagh-kube ; git pull)
 else
-    git clone https://github.com/blablack/fawnarevagh-kube.git
+    (cd /data/ ; git clone https://github.com/blablack/fawnarevagh-kube.git)
 fi
 
 (
-    cd fawnarevagh-kube/docker-builder 
-    podman --root /data build -t kublicity . 
+    cd /data/fawnarevagh-kube/docker-builder 
+    podman --root /data/podman build -t kublicity . 
     podman tag docker-builder:latest nucio.nowhere:30038/docker-builder:latest
     podman push nucio.nowhere:30038/docker-builder:latest
 )
 
 (
-    cd fawnarevagh-kube/kublicity 
-    podman --root /data build -t kublicity . 
+    cd /data/fawnarevagh-kube/kublicity 
+    podman --root /data/podman build -t kublicity . 
     podman tag kublicity:latest nucio.nowhere:30038/kublicity:latest
     podman push nucio.nowhere:30038/kublicity:latest
 )
 
 (
-    cd fawnarevagh-kube/picsync 
-    podman --root /data build -t picsync . 
+    cd /data/fawnarevagh-kube/picsync 
+    podman --root /data/podman build -t picsync . 
     podman tag picsync:latest nucio.nowhere:30038/picsync:latest
     podman push nucio.nowhere:30038/picsync:latest
 )
 
 (
-    cd nordvpn/nordvpn
-    podman --root /data build -t nordvpn . 
+    cd /data/nordvpn/nordvpn
+    podman --root /data/podman build -t nordvpn . 
     podman tag nordvpn:latest nucio.nowhere:30038/nordvpn:latest
     podman push nucio.nowhere:30038/nordvpn:latest
 )
