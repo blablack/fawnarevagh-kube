106fdf8715d5412d1e66986aef3db71bd670a654
Aurélien Leblond <blablack@gmail.com>
2022-11-06 23:02:41 +0000

    NordVPN Deluge

--- nordvpn/run_nordvpn.sh
+++ nordvpn/run_nordvpn.sh
@@ -9,12 +9,22 @@ nordvpn login --token $NORDVPN_TOKEN &&
     
 echo "Configure NordVPN"
 nordvpn set technology nordlynx && 
+
 nordvpn whitelist add subnet 192.168.2.0/24 &&
+nordvpn whitelist add subnet 10.42.0.0/24 &&
+
+nordvpn whitelist add port 8080 protocol TCP &&
+nordvpn whitelist add port 8112 protocol TCP &&
+nordvpn whitelist add port 6881 &&
+
+nordvpn whitelist add port  &&
+
 nordvpn set dns 192.168.2.200 &&
     
 echo "Connect to NordVPN"
 nordvpn connect $COUNTRY && 
 
+nordvpn settings &&
 nordvpn status &&
 
 while nordvpn status | grep -q 'Status: Connected' ; 
