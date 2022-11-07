953b4e0368802e491467f52abe67f8f61ccea8e8
Aurélien Leblond <blablack@gmail.com>
2022-11-07 11:02:55 +0000

    Updated NordVPN

--- nordvpn/run_nordvpn.sh
+++ nordvpn/run_nordvpn.sh
@@ -1,32 +1,142 @@
 #!/bin/bash
+set -x
 
-echo "Start NordVPN Service"
-systemctl start nordvpn && 
-sleep 2s && 
+[[ -n ${COUNTRY} && -z ${CONNECT} ]] && CONNECT=${COUNTRY}
 
-echo "Login to NordVPN"
-nordvpn login --token $NORDVPN_TOKEN &&
-    
-echo "Configure NordVPN"
-nordvpn set technology nordlynx && 
+DOCKER_NET="$(ip -o addr show dev eth0 | awk '$3 == "inet" {print $4}')" 
 
-nordvpn whitelist add subnet 192.168.2.0/24 &&
-nordvpn whitelist add subnet 10.42.0.0/24 &&
+kill_switch() {
+	iptables -F
+	iptables -X
+	iptables -P INPUT DROP
+	iptables -P FORWARD DROP
+	iptables -P OUTPUT DROP
+	iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+	iptables -A INPUT -i lo -j ACCEPT
+	iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+	iptables -A FORWARD -i lo -j ACCEPT
+	iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+	iptables -A OUTPUT -o lo -j ACCEPT
+	iptables -A OUTPUT -o tap+ -j ACCEPT
+	iptables -A OUTPUT -o tun+ -j ACCEPT
+    iptables -A OUTPUT -o nordlynx -j ACCEPT
+    iptables -t nat -A POSTROUTING -o tap+ -j MASQUERADE
+	iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+	iptables -t nat -A POSTROUTING -o nordlynx -j MASQUERADE
 
-nordvpn whitelist add port 8080 protocol TCP &&
-nordvpn whitelist add port 8112 protocol TCP &&
-nordvpn whitelist add port 6881 &&
+    iptables  -A OUTPUT -p udp -m udp --dport 53    -j ACCEPT
+    iptables  -A OUTPUT -p udp -m udp --dport 51820 -j ACCEPT
+    iptables  -A OUTPUT -p tcp -m tcp --dport 1194  -j ACCEPT
+    iptables  -A OUTPUT -p udp -m udp --dport 1194  -j ACCEPT
+    iptables  -A OUTPUT -p tcp -m tcp --dport 443   -j ACCEPT
 
-nordvpn whitelist add port  &&
+	if [[ -n ${DOCKER_NET} ]]; then
+		iptables -A INPUT -s "${DOCKER_NET}" -j ACCEPT
+		iptables -A FORWARD -d "${DOCKER_NET}" -j ACCEPT
+		iptables -A FORWARD -s "${DOCKER_NET}" -j ACCEPT
+		iptables -A OUTPUT -d "${DOCKER_NET}" -j ACCEPT
+	fi
+	[[ -n ${NETWORK} ]]  && for net in ${NETWORK//[;,]/ };  do return_route "${net}";  done
 
-nordvpn set dns 192.168.2.200 &&
-    
-echo "Connect to NordVPN"
-nordvpn connect $COUNTRY && 
+	ip6tables -F 2>/dev/null
+	ip6tables -X 2>/dev/null
+	ip6tables -P INPUT DROP 2>/dev/null
+	ip6tables -P FORWARD DROP 2>/dev/null
+	ip6tables -P OUTPUT DROP 2>/dev/null
+	ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
+	ip6tables -A INPUT -p icmp -j ACCEPT 2>/dev/null
+	ip6tables -A INPUT -i lo -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -p icmp -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -i lo -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -o lo -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -o tap+ -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -o tun+ -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -o nordlynx -j ACCEPT 2>/dev/null
 
-nordvpn settings &&
-nordvpn status &&
+    ip6tables -A OUTPUT -p udp -m udp --dport 53    -j ACCEPT 2>/dev/null
+    ip6tables -A OUTPUT -p udp -m udp --dport 51820 -j ACCEPT 2>/dev/null
+    ip6tables -A OUTPUT -p tcp -m tcp --dport 1194  -j ACCEPT 2>/dev/null
+    ip6tables -A OUTPUT -p udp -m udp --dport 1194  -j ACCEPT 2>/dev/null
+    ip6tables -A OUTPUT -p tcp -m tcp --dport 443   -j ACCEPT 2>/dev/null
+
+    local docker6_network="$(ip -o addr show dev eth0 | awk '$3 == "inet6" {print $4; exit}')"
+	if [[ -n ${docker6_network} ]]; then
+		ip6tables -A INPUT -s "${docker6_network}" -j ACCEPT 2>/dev/null
+		ip6tables -A FORWARD -d "${docker6_network}" -j ACCEPT 2>/dev/null
+		ip6tables -A FORWARD -s "${docker6_network}" -j ACCEPT 2>/dev/null
+		ip6tables -A OUTPUT -d "${docker6_network}" -j ACCEPT 2>/dev/null
+	fi
+	[[ -n ${NETWORK6} ]] && for net in ${NETWORK6//[;,]/ }; do return_route6 "${net}"; done
+
+  white_list "api.nordvpn.com"
+  [[ -n ${WHITELIST} ]] && for domain in ${WHITELIST//[;,]/ }; do white_list "${domain}"; done
+}
+
+return_route() { # Add a route back to your network, so that return traffic works
+	local network="$1" gw=$(ip route |awk '/default/ {print $3}')
+    ip route | grep -q "$network" || ip route add to "$network" via "$gw" dev eth0
+    iptables -A INPUT -s "$network" -j ACCEPT
+    iptables -A FORWARD -d "$network" -j ACCEPT
+    iptables -A FORWARD -s "$network" -j ACCEPT
+    iptables -A OUTPUT -d "$network" -j ACCEPT
+}
+
+return_route6() { # Add a route back to your network, so that return traffic works
+	local network="$1" gw=$(ip -6 route | awk '/default/{print $3}')
+	ip -6 route | grep -q "$network" || ip -6 route add to "$network" via "$gw" dev eth0
+	ip6tables -A INPUT -s "$network" -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -d "$network" -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -s "$network" -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -d "$network" -j ACCEPT 2>/dev/null
+}
+
+white_list() { # Allow unsecured traffic for an specific domain
+	local domain=$(echo "$1" | sed 's/^.*:\/\///;s/\/.*$//')
+	iptables  -A OUTPUT -o eth0 -d ${domain} -j ACCEPT
+	ip6tables -A OUTPUT -o eth0 -d ${domain} -j ACCEPT 2>/dev/null
+}
+
+setup_nordvpn() {
+    pkill nordvpnd
+    mkdir -p /run/nordvpn
+    rm -f /run/nordvpn/nordvpnd.sock
+    nordvpnd &
+    while [ ! -S /run/nordvpn/nordvpnd.sock ]; do
+        sleep 0.25
+    done
+
+    nordvpn login --token $NORDVPN_TOKEN &&
+
+    nordvpn set technology nordlynx && 
+	[[ -n ${DNS} ]] && nordvpn set dns ${DNS//[;,]/ }
+	[[ -n ${DOCKER_NET} ]] && nordvpn whitelist add subnet ${DOCKER_NET}
+	[[ -n ${NETWORK} ]] && for net in ${NETWORK//[;,]/ };  do nordvpn whitelist add subnet "${net}";  done
+	[[ -n ${PORTS} ]] && for port in ${PORTS//[;,]/ };  do nordvpn whitelist add port "${port}";  done
+	
+    nordvpn -version && nordvpn settings
+
+	mkdir -p /dev/net
+	[[ -c /dev/net/tun ]] || mknod -m 0666 /dev/net/tun c 10 200
+}
+
+cleanup() {
+	nordvpn disconnect
+	pkill nordvpnd
+	trap - SIGTERM SIGINT EXIT 
+	exit 0
+}
+trap cleanup SIGTERM SIGINT EXIT
+
+kill_switch
+setup_nordvpn
+
+nordvpn connect ${CONNECT} || exit 1
+
+nordvpn status
 
 while nordvpn status | grep -q 'Status: Connected' ; 
     do sleep 5m ; 
 done
+
