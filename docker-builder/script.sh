fbca82b2fbff927d77e2bfe8d8a02a88caa073c6
Aurélien Leblond <blablack@gmail.com>
2022-11-12 14:35:21 +0000

    Build in docker format

--- docker-builder/script.sh
+++ docker-builder/script.sh
@@ -12,28 +12,28 @@ mkdir -p /data/podman
 
 (
     cd /data/fawnarevagh-kube/docker-builder 
-    podman build -t docker-builder . 
+    podman build --format docker -t docker-builder . 
     podman tag docker-builder:latest nucio.nowhere:30038/docker-builder:latest
     podman push nucio.nowhere:30038/docker-builder:latest
 )
 
 (
     cd /data/fawnarevagh-kube/kublicity 
-    podman build -t kublicity . 
+    podman build --format docker -t kublicity . 
     podman tag kublicity:latest nucio.nowhere:30038/kublicity:latest
     podman push nucio.nowhere:30038/kublicity:latest
 )
 
 (
     cd /data/fawnarevagh-kube/picsync 
-    podman build -t picsync . 
+    podman build --format docker -t picsync . 
     podman tag picsync:latest nucio.nowhere:30038/picsync:latest
     podman push nucio.nowhere:30038/picsync:latest
 )
 
 (
     cd /data/fawnarevagh-kube/nordvpn
-    podman build -t nordvpn . 
+    podman build --format docker -t nordvpn . 
     podman tag nordvpn:latest nucio.nowhere:30038/nordvpn:latest
     podman push nucio.nowhere:30038/nordvpn:latest
 )
