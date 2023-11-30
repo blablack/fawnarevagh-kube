51376ea481dc079d34a48053e88a5b3a195e79de
Aurélien Leblond <blablack@gmail.com>
2023-11-30 00:15:50 +0000

    Tmp commit

--- docker/nordvpn/add_to_meshnet.sh
+++ docker/nordvpn/add_to_meshnet.sh
@@ -2,7 +2,7 @@
 
 peer_list=$(nordvpn meshnet peer list)
 
-for i in $(echo "$peer_list" | grep 'Hostname:' | awk -F ': ' 'NR==2{print $2}'); do
+for i in $(echo "$peer_list" | awk '/Hostname:/ && NR > 2 {print $2}'); do
     # print the hostname
     nordvpn meshnet peer routing allow $i
     nordvpn meshnet peer local allow $i
