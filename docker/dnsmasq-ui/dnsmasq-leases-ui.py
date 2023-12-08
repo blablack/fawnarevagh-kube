69cd5c376dfae5e757246b445ea1f967cfccd456
Aurélien Leblond <blablack@gmail.com>
2023-12-08 16:18:50 +0000

    dnsmasq-ui

--- docker/dnsmasq-ui/dnsmasq-leases-ui.py
+++ docker/dnsmasq-ui/dnsmasq-leases-ui.py
@@ -10,24 +10,18 @@ app = Flask(__name__)
 
 class LeaseEntry:
     def __init__(self, leasetime, macAddress, ipAddress, name):
-        self.leasetime = datetime.datetime.fromtimestamp(int(leasetime)).strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
+        self.leasetime = datetime.datetime.fromtimestamp(int(leasetime))
         self.macAddress = macAddress.upper()
         self.ipAddress = ipAddress
         self.name = name
 
     def serialize(self):
         return {
-            "leasetime": self.leasetime,
+            "leasetime": self.leasetime.strftime("%Y-%m-%d %H:%M:%S"),
             "macAddress": self.macAddress,
             "ipAddress": self.ipAddress,
             "name": self.name,
         }
-
-
-def leaseSort(arg):
-    return arg.ipAddress
 
 
 @app.route("/")
@@ -37,16 +31,15 @@ def index():
 
 @app.route("/leases")
 def getLeases():
-    leases = list()
+    leases = []
     with open(DNSMASQ_LEASES_FILE) as f:
-        for line in f:
-            elements = line.split()
-            if len(elements) == 5:
-                entry = LeaseEntry(elements[0], elements[1], elements[2], elements[3])
-                leases.append(entry)
+        leases = [
+            LeaseEntry(elements[0], elements[1], elements[2], elements[3]).serialize()
+            for line in f
+            if (elements := line.split()) and len(elements) == 5
+        ]
 
-    leases.sort(key=leaseSort)
-    return jsonify(leases=[lease.serialize() for lease in leases])
+    return jsonify(leases=leases)
 
 
 if __name__ == "__main__":
