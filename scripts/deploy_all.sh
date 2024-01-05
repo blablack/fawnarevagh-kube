459095e5334037f87c013241a51c5d873b88ebda
Aurélien Leblond <blablack@gmail.com>
2024-01-05 12:37:16 +0000

    deploy_all script fixed

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -1,53 +1,62 @@
 #!/bin/bash
 
-kubectl apply -f ../metallb/metallb-config.yaml
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+kubectl apply -f $SCRIPT_DIR/../metallb/metallb-config.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/config/manifests/metallb-native.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.5.3/deploy/longhorn.yaml
-kubectl apply -f ../longhorn/longhorn.yaml
+kubectl apply -f $SCRIPT_DIR/../longhorn/longhorn.yaml
 
-kubectl apply -f ../persistent-volumes/nasio-nfs.yaml
-kubectl apply -f ../persistent-volumes/longhorn.yaml
+kubectl apply -f $SCRIPT_DIR/../persistent-volumes/nasio-nfs.yaml
+kubectl apply -f $SCRIPT_DIR/../persistent-volumes/longhorn.yaml
 
-kubectl apply -f ../registry/registry-pvc.yaml
-kubectl apply -f ../registry/registry.yaml
+kubectl apply -f $SCRIPT_DIR/../registry/registry-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../registry/registry.yaml
 
-kubectl apply -f ../download-root-hints/download-root-hints.yaml
-kubectl apply -f ../pihole/pihole.yaml
+kubectl apply -f $SCRIPT_DIR/../pihole/pihole.yaml
 
-kubectl apply -f ../dnsmasq/dnsmasq-pvc.yaml
-kubectl apply -f ../dnsmasq/dnsmasq.yaml
+kubectl apply -f $SCRIPT_DIR/../dnsmasq/dnsmasq-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../dnsmasq/dnsmasq.yaml
 
-kubectl apply -f ../jenkins/jenkins-pvc.yaml
-kubectl apply -f ../jenkins/jenkins.yaml
+kubectl apply -f $SCRIPT_DIR/../jenkins/jenkins-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../jenkins/jenkins.yaml
 
-kubectl apply -f ../syncthing/syncthing-aurelien-pvc.yaml
-kubectl apply -f ../syncthing/syncthing-aurelien.yaml
-kubectl apply -f ../syncthing/syncthing-yvonne-pvc.yaml
-kubectl apply -f ../syncthing/syncthing-yvonne.yaml
+kubectl apply -f $SCRIPT_DIR/../syncthing/syncthing-aurelien-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../syncthing/syncthing-aurelien.yaml
+kubectl apply -f $SCRIPT_DIR/../syncthing/syncthing-yvonne-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../syncthing/syncthing-yvonne.yaml
 
-kubectl apply -f ../immich/immich-pvc.yaml
-kubectl apply -f ../immich/immich.yaml
+kubectl apply -f $SCRIPT_DIR/../immich/immich-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../immich/immich.yaml
 
-kubectl apply -f ../heimdall/heimdall-pvc.yaml
-kubectl apply -f ../heimdall/heimdall.yaml
+kubectl apply -f $SCRIPT_DIR/../heimdall/heimdall-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../heimdall/heimdall.yaml
 
-kubectl apply -f ../home-assistant/home-assistant-pvc.yaml
-kubectl apply -f ../home-assistant/home-assistant.yaml
-kubectl apply -f ../node-red/node-red-pvc.yaml
-kubectl apply -f ../node-red/node-red.yaml
+kubectl apply -f $SCRIPT_DIR/../homer/homer-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../homer/homer.yaml
 
-kubectl apply -f ../plex/plex-pvc.yaml
-kubectl apply -f ../plex/plex.yaml
-kubectl apply -f ../sonarr/sonarr-pvc.yaml
-kubectl apply -f ../sonarr/sonarr.yaml
-kubectl apply -f ../radarr/radarr-pvc.yaml
-kubectl apply -f ../radarr/radarr.yaml
-kubectl apply -f ../nordvpn/nordvpn-pvc.yaml
-kubectl apply -f ../nordvpn/nordvpn.yaml
-kubectl apply -f ../qbittorrent/qbittorrent-pvc.yaml
-kubectl apply -f ../qbittorrent/qbittorrent.yaml
-kubectl apply -f ../prowlarr/prowlarr-pvc.yaml
-kubectl apply -f ../prowlarr/prowlarr.yaml
+kubectl apply -f $SCRIPT_DIR/../paperless/paperless-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../paperless/paperless.yaml
 
-kubectl apply -f ../picsync/picsync.yaml
+kubectl apply -f $SCRIPT_DIR/../home-assistant/home-assistant-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../home-assistant/home-assistant.yaml
+kubectl apply -f $SCRIPT_DIR/../node-red/node-red-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../node-red/node-red.yaml
+
+kubectl apply -f $SCRIPT_DIR/../plex/plex-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../plex/plex.yaml
+
+kubectl apply -f $SCRIPT_DIR/../sonarr/sonarr-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../sonarr/sonarr.yaml
+kubectl apply -f $SCRIPT_DIR/../radarr/radarr-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../radarr/radarr.yaml
+kubectl apply -f $SCRIPT_DIR/../nordvpn/nordvpn-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../nordvpn/nordvpn.yaml
+kubectl apply -f $SCRIPT_DIR/../qbittorrent/qbittorrent-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../qbittorrent/qbittorrent.yaml
+kubectl apply -f $SCRIPT_DIR/../prowlarr/prowlarr-pvc.yaml
+kubectl apply -f $SCRIPT_DIR/../prowlarr/prowlarr.yaml
+
+kubectl apply -f $SCRIPT_DIR/../picsync/picsync-immich.yaml
+kubectl apply -f $SCRIPT_DIR/../picsync/picsync-legacy.yaml
