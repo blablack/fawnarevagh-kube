fae076433c7bca7a998c0bd5cddf3b8a0a6b8c26
Aurélien Leblond <blablack@gmail.com>
2024-02-06 23:33:04 +0000

    Fix script

--- docker/text-generation-webui-init/script.sh
+++ docker/text-generation-webui-init/script.sh
@@ -3,8 +3,15 @@
 CONFIG_DIRECTORIES=("characters" "loras" "models" "presets" "prompts" "training/datasets" "training/formats")
 
 for config_dir in "${CONFIG_DIRECTORIES[@]}"; do
-  if [ -z "$(ls /home/app/text-generation-webui/"$config_dir")" ]; then
-    echo "*** Initialising config for: '$config_dir' ***"
-    cp -ar /text-generation-webui/"$config_dir"/* /home/app/text-generation-webui/"$config_dir"/
+  target_dir="/home/app/text-generation-webui/$config_dir"
+
+  if [ ! -d "$target_dir" ]; then
+    echo "*** Creating directory: '$target_dir' ***"
+    mkdir -p "$target_dir"
+  fi
+
+  if [ -z "$(ls "$target_dir")" ]; then
+    echo "*** Initializing config for: '$config_dir' ***"
+    cp -ar "/text-generation-webui/$config_dir"/* "$target_dir"/
   fi
 done
