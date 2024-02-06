7fd5eeaf04a779219821267763b1db882a3640db
Aurélien Leblond <blablack@gmail.com>
2024-02-06 23:14:53 +0000

    Fixes scripts

--- docker/text-generation-webui-init/script.sh
+++ docker/text-generation-webui-init/script.sh
@@ -3,7 +3,7 @@
 CONFIG_DIRECTORIES=("characters" "loras" "models" "presets" "prompts" "training/datasets" "training/formats")
 
 for config_dir in "${CONFIG_DIRECTORIES[@]}"; do
-  if [ -z "$(ls /app/"$config_dir")" ]; then
+  if [ -z "$(ls /home/app/text-generation-webui/"$config_dir")" ]; then
     echo "*** Initialising config for: '$config_dir' ***"
     cp -ar /text-generation-webui/"$config_dir"/* /home/app/text-generation-webui/"$config_dir"/
   fi
