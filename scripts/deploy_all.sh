dbeb9afad10859ab384f139821c7e77a583b48fd
Aurélien Leblond <blablack@gmail.com>
2024-08-18 18:26:38 +0100

    Version update

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 kubectl apply -f $SCRIPT_DIR/../metallb/metallb-config.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.6.2/deploy/longhorn.yaml
 kubectl apply -f $SCRIPT_DIR/../longhorn/longhorn.yaml
