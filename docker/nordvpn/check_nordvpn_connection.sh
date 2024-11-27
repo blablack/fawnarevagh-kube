51ad77b2d253b69302d171299e9d7181d184bea9
Aurélien Leblond <blablack@gmail.com>
2024-11-27 16:12:47 +0000

    New check connection

--- docker/nordvpn/check_nordvpn_connection.sh
+++ docker/nordvpn/check_nordvpn_connection.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-status=$(curl --interface nordlynx -m 10 -s https://api.nordvpn.com/v1/helpers/ips/insights | jq -r '.["protected"]')
-if [ "$status" == "true" ]; then 
-    exit 0
-else 
-    exit 1
+#status=$(curl --interface nordlynx -m 10 -s https://api.nordvpn.com/v1/helpers/ips/insights | jq -r '.["protected"]')
+#if [ "$status" == "true" ]; then 
+#    exit 0
+#else 
+#    exit 1
+#fi
+
+status_output=$(nordvpn status)
+
+if echo "$status_output" | grep -q 'Status: Connected'; then
+  exit 0
+else
+  exit 1
 fi
