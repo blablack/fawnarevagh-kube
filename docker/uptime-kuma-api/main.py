f6e768b6f3c68645317f21e7a96039bf79869940
Aurélien Leblond <blablack@gmail.com>
2025-01-16 11:07:42 +0000

    Better Kuma API

--- docker/uptime-kuma-api/main.py
+++ docker/uptime-kuma-api/main.py
@@ -1,19 +1,21 @@
 import os
 import logging
+import time
 
 from fastapi import FastAPI, HTTPException
 from uptime_kuma_api import UptimeKumaApi
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 app = FastAPI()
 
 logging.basicConfig(
-    level=logging.DEBUG,  # Set the log level
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Define the log format
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger("Uptime Kuma API")
 
 
-def get_api():
+def _get_api():
     kuma_url = os.getenv("KUMA_URL")
     username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")
@@ -36,27 +38,63 @@ def get_api():
     return api
 
 
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(30))
+def _pause_monitor(monitor_id: int):
+    try:
+        api = _get_api()
+        answer = api.pause_monitor(monitor_id)
+        logger.info(f"Attempted tp pause monitor with ID: {monitor_id}")
+        logger.info(answer)
+
+        time.sleep(10)
+
+        monitor_status = api.get_monitor(monitor_id)
+        if monitor_status.get("active") == 0:
+            logger.info(f"Successfully paused monitor with ID: {monitor_id}")
+        else:
+            raise Exception(
+                f"Failed to pause monitor with ID: {monitor_id}. Monitor is still active."
+            )
+    except:
+        raise
+
+
 @app.post("/pause_monitor/{monitor_id}")
 async def pause_monitor(monitor_id: int):
     logger.info(f"Received request to pause monitor with ID: {monitor_id}")
     try:
-        api = get_api()
-        answer = api.pause_monitor(monitor_id)
-        logger.info(f"Successfully paused monitor with ID: {monitor_id}")
-        logger.info(answer)
+        _pause_monitor(monitor_id)
     except Exception as e:
         logger.error(f"Failed to pause monitor with ID {monitor_id}: {e}")
         raise HTTPException(status_code=500, detail=f"Error pausing monitor: {str(e)}")
+
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(30))
+def _resume_monitor(monitor_id: int):
+    try:
+        api = _get_api()
+        answer = api.resume_monitor(monitor_id)
+        logger.info(f"Attempted to resume monitor with ID: {monitor_id}")
+        logger.info(answer)
+
+        time.sleep(10)
+
+        monitor_status = api.get_monitor(monitor_id)
+        if monitor_status.get("active") == 1:
+            logger.info(f"Successfully resumed monitor with ID: {monitor_id}")
+        else:
+            raise Exception(
+                f"Failed to resume monitor with ID: {monitor_id}. Monitor is still paused."
+            )
+    except:
+        raise
 
 
 @app.post("/resume_monitor/{monitor_id}")
 async def resume_monitor(monitor_id: int):
     logger.info(f"Received request to resume monitor with ID: {monitor_id}")
     try:
-        api = get_api()
-        answer = api.resume_monitor(monitor_id)
-        logger.info(f"Successfully resumed monitor with ID: {monitor_id}")
-        logger.info(answer)
+        _resume_monitor(monitor_id)
     except Exception as e:
         logger.error(f"Failed to resume monitor with ID {monitor_id}: {e}")
         raise HTTPException(status_code=500, detail=f"Error resuming monitor: {str(e)}")
