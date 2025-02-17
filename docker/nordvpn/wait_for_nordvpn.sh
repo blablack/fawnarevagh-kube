ca95df7c68c63436b3735a4a0e1aa13f3cc53e02
Aurélien Leblond <blablack@gmail.com>
2025-02-17 16:30:01 +0000

    Code cleanup

--- docker/nordvpn/wait_for_nordvpn.sh
+++ docker/nordvpn/wait_for_nordvpn.sh
@@ -2,11 +2,11 @@
 
 sleep 10s
 
-while ! nordvpn status | grep -q 'Status: Connected' ; 
-    do sleep 10s ; 
+while ! nordvpn status | grep -q 'Status: Connected'; do
+    sleep 10s
 done
 
-echo `date`
+echo $(date)
 echo "NordVPN is connected"
 
 sleep 1m
@@ -14,4 +14,4 @@ sleep 1m
 nordvpn status
 
 echo "NordVPN is initialized"
-echo `date`
+echo $(date)
--- scripts/bash_to_container.sh
+++ scripts/bash_to_container.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-POD=`kubectl get pods -l=app=$1 | tail -n 1 | cut -d ' ' -f1`
+POD=$(kubectl get pods -l=app=$1 | tail -n 1 | cut -d ' ' -f1)
 
-if [ -z "$2" ]
-  then
-    kubectl exec --stdin --tty $POD -- /bin/bash
-  else
-	kubectl exec --stdin --tty $POD -c $2 -- /bin/bash
+if [ -z "$2" ]; then
+  kubectl exec --stdin --tty $POD -- /bin/bash
+else
+  kubectl exec --stdin --tty $POD -c $2 -- /bin/bash
 fi
