2a77422a2a1b570353b10515e1b8f951e1c51ac5
Aurélien Leblond <blablack@gmail.com>
2025-07-11 19:35:33 +0100

    Cleaner with kustomize!

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -18,13 +18,7 @@ kubectl apply -f $SCRIPT_DIR/../registry/registry.yaml
 
 (
     cd $SCRIPT_DIR/../intel-gpu-plugin
-    wget -O intel-gpu-plugin.yaml https://raw.githubusercontent.com/intel/intel-device-plugins-for-kubernetes/refs/heads/main/deployments/gpu_plugin/base/intel-gpu-plugin.yaml
-    sed -i 's/imagePullPolicy: IfNotPresent/imagePullPolicy: Always/g' intel-gpu-plugin.yaml
-    sed -i '/imagePullPolicy: Always/a\
-        args:\
-          - "-shared-dev-num"\
-          - "2"' ./intel-gpu-plugin.yaml
-    kubectl apply -f intel-gpu-plugin.yaml
+    kubectl apply -k .
 )
 
 (
@@ -32,9 +26,8 @@ kubectl apply -f $SCRIPT_DIR/../registry/registry.yaml
 
     kubectl apply -f argocd-namespace.yaml
 
-    wget https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -O argocd.yaml
-
-    kubectl apply --force-conflicts --server-side --namespace argocd -f argocd.yaml
+    kubectl apply -k .
+    
     kubectl apply --namespace argocd -f argocd-service.yaml
     kubectl apply --namespace argocd -f argocd-config.yaml
     kubectl patch configmap argocd-cm -n argocd --patch-file argocd-dex-config.yaml
@@ -46,9 +39,7 @@ kubectl apply -f $SCRIPT_DIR/../registry/registry.yaml
 
     kubectl apply -f namespace.yaml
 
-    wget -O prometheus-crd.yaml https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/master/bundle.yaml
-    sed -i 's/namespace: default/namespace: monitoring/g' prometheus-crd.yaml
-    kubectl apply -f prometheus-crd.yaml --server-side --force-conflicts
+    kubectl apply -k . --server-side --force-conflicts
 
     kubectl apply -f prometheus-rbac.yaml
 
