eefc16e3e08b69e88146ef2cee3965cac39c346a
Aurélien Leblond <blablack@gmail.com>
2025-07-12 23:35:32 +0100

    fix script

--- scripts/deploy_one.sh
+++ scripts/deploy_one.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PV=$(kubectl get pv -o json | jq -r ".items[] | select(.spec.claimRef != null and (.spec.claimRef.name | contains(\"$1\"))) | .metadata.name")
 kubectl patch pv $PV -p '{"spec":{"claimRef":null}}'
 
+echo $PV
+
 (
-    cd $SCRIPT_DIR/../$i
+    cd $SCRIPT_DIR/../$1
     for i in *.yaml ; do kubectl apply -f $i ; done
 )
