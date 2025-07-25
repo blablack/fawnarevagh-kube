1773bcbb14f8376737aa9b977894d4d4d0fe9243
Aurélien Leblond <blablack@gmail.com>
2025-07-25 15:05:30 +0100

    cleanup

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -13,9 +13,6 @@ kubectl apply -f $SCRIPT_DIR/../longhorn/longhorn.yaml
 kubectl apply -f $SCRIPT_DIR/../persistent-volumes/nasio-nfs.yaml
 kubectl apply -f $SCRIPT_DIR/../persistent-volumes/longhorn.yaml
 
-kubectl apply -f $SCRIPT_DIR/../registry/registry-pvc.yaml
-kubectl apply -f $SCRIPT_DIR/../registry/registry.yaml
-
 (
     cd $SCRIPT_DIR/../intel-gpu-plugin
     kubectl apply -k .
@@ -23,31 +20,11 @@ kubectl apply -f $SCRIPT_DIR/../registry/registry.yaml
 
 (
     cd $SCRIPT_DIR/../argocd
-
-    kubectl apply -f argocd-namespace.yaml
-
-    kubectl apply -k .
-    
-    kubectl apply --namespace argocd -f argocd-service.yaml
-    kubectl apply --namespace argocd -f argocd-config.yaml
-    kubectl patch configmap argocd-cm -n argocd --patch-file argocd-dex-config.yaml
-    kubectl apply --namespace argocd -f argocd-applicationset.yaml
+    kubectl apply -k .    
 )
 
 (
     cd $SCRIPT_DIR/../prometheus
-
-    kubectl apply -f namespace.yaml
-
     kubectl apply -k . --server-side --force-conflicts
-
-    kubectl apply -f prometheus-rbac.yaml
-
-    kubectl apply -f prometheus.yaml
-
-    kubectl apply -f monitor-node-exporter.yaml
-    kubectl apply -f monitor-longhorn.yaml
-    kubectl apply -f monitor-kubelet.yaml
-    kubectl apply -f monitor-snmp.yaml
 )
 
