52b59a2e363d5fd9410884b7facc012689c88d15
Aurélien Leblond <blablack@gmail.com>
2025-08-22 19:18:56 +0100

    need cleanup

--- ansible/all/dns-switcher.sh
+++ ansible/all/dns-switcher.sh
@@ -6,6 +6,8 @@ while true; do
         if [ "$current_dns" != "192.168.2.201 192.168.2.202" ]; then
             sed -i 's/^DNS=.*/DNS=192.168.2.201 192.168.2.202/' /etc/systemd/resolved.conf
             systemctl restart systemd-resolved
+            sleep 15
+            kubectl -n kube-system rollout restart deployment coredns
         fi
     else
         # Pi-hole is down, use fallback
@@ -13,6 +15,8 @@ while true; do
         if [ "$current_dns" != "1.1.1.1 1.0.0.1" ]; then
             sed -i 's/^DNS=.*/DNS=1.1.1.1 1.0.0.1/' /etc/systemd/resolved.conf
             systemctl restart systemd-resolved
+            sleep 15
+            kubectl -n kube-system rollout restart deployment coredns
         fi
     fi
     sleep 3
