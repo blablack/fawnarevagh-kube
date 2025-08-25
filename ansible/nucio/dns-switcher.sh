cc6fb1a8c454cc8eb947fbc2d4b97dd5cfaa683d
Aurélien Leblond <blablack@gmail.com>
2025-08-25 16:49:35 +0100

    simplification

--- ansible/nucio/dns-switcher.sh
+++ ansible/nucio/dns-switcher.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 while true; do
-    if nc -zv 192.168.2.201 53 2>/dev/null; then
+    if nc -zv 192.168.2.2 53 2>/dev/null; then
         # Pi-hole is up, use it
         current_dns=$(grep "^DNS=" /etc/systemd/resolved.conf | cut -d= -f2)
-        if [ "$current_dns" != "192.168.2.201 192.168.2.202" ]; then
-            sed -i 's/^DNS=.*/DNS=192.168.2.201 192.168.2.202/' /etc/systemd/resolved.conf
+        if [ "$current_dns" != "192.168.2.2" ]; then
+            sed -i 's/^DNS=.*/DNS=192.168.2.2/' /etc/systemd/resolved.conf
             systemctl restart systemd-resolved
             sleep 15
             kubectl -n kube-system rollout restart deployment coredns
