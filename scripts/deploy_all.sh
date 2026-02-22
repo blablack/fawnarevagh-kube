7a60d731350b5c10b50cae3c456a0e6070af4bf4
Aurélien Leblond <blablack@gmail.com>
2026-02-22 12:18:08 +0000

    versions

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -14,7 +14,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.15.3/config/manifests/metallb-native.yaml
 kubectl apply -f $SCRIPT_DIR/../metallb/metallb-config.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.10.2/deploy/longhorn.yaml
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.11.0/deploy/longhorn.yaml
 kubectl apply -f $SCRIPT_DIR/../longhorn/longhorn.yaml
 
 kubectl apply -f $SCRIPT_DIR/../persistent-volumes/nasio-nfs.yaml
@@ -27,11 +27,10 @@ kubectl apply -f $SCRIPT_DIR/../persistent-volumes/longhorn.yaml
 
 (
     cd $SCRIPT_DIR/../argocd
-    kubectl apply -k .    
+    kubectl apply --server-side --force-conflicts -k .
 )
 
 (
     cd $SCRIPT_DIR/../prometheus
-    kubectl apply -k . --server-side --force-conflicts
-)
+    kubectl apply --server-side --force-conflicts -k .
 
