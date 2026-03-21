be9a4ebec41ccbde239dc98d4211fd918ecfc70b
Aurélien Leblond <blablack@gmail.com>
2026-03-21 18:53:59 +0000

    update

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -23,7 +23,7 @@ kubectl apply -f $SCRIPT_DIR/../cert-manager/cert-manager.yaml
 kubectl apply -f $SCRIPT_DIR/../persistent-volumes/nasio-nfs.yaml
 kubectl apply -f $SCRIPT_DIR/../persistent-volumes/longhorn.yaml
 
-kubectl annotate svc traefik -n kube-system metallb.io/loadBalancerIPs=192.168.2.200
+kubectl annotate svc traefik -n kube-system metallb.io/loadBalancerIPs=192.168.2.220
 
 (
     cd $SCRIPT_DIR/../intel-gpu-plugin
