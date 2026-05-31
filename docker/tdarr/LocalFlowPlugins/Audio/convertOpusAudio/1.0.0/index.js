1ce1ee3801dc3009d00cf8caeb6bb1afd52b8474
Aurélien Leblond <blablack@gmail.com>
2026-05-31 17:29:34 +0100

    Add Convert OPUS Audio plugin and update shield flow configuration

---
+++ docker/tdarr/LocalFlowPlugins/Audio/convertOpusAudio/1.0.0/index.js
@@ -0,0 +1,156 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = op[0] ? t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.plugin = exports.details = void 0;
+var cliUtils_1 = require("../../../../FlowHelpers/1.0.0/cliUtils");
+var fileUtils_1 = require("../../../../FlowHelpers/1.0.0/fileUtils");
+
+var details = function () { return ({
+    name: 'Convert OPUS Audio',
+    description: 'Converts OPUS audio streams to a specified codec (default: eac3) while copying all other streams. '
+        + 'Outputs on path 1 if OPUS was found and converted, path 2 if no OPUS streams exist.',
+    style: {
+        borderColor: '#ff9900',
+    },
+    tags: 'audio',
+    isStartPlugin: false,
+    pType: '',
+    requiresVersion: '2.11.01',
+    sidebarPosition: -1,
+    icon: '',
+    inputs: [
+        {
+            label: 'Output Codec',
+            name: 'outputCodec',
+            type: 'string',
+            defaultValue: 'eac3',
+            inputUI: { type: 'text' },
+            tooltip: 'Target codec for OPUS audio streams (e.g. eac3, aac, ac3)',
+        },
+    ],
+    outputs: [
+        {
+            number: 1,
+            tooltip: 'OPUS streams found and converted',
+        },
+        {
+            number: 2,
+            tooltip: 'No OPUS streams found, file unchanged',
+        },
+    ],
+}); };
+exports.details = details;
+
+var plugin = function (args) { return __awaiter(void 0, void 0, void 0, function () {
+    var lib, outputCodec, streams, opusAudioIndices, audioIdx, codecOverrides, path, ext, pluginWorkDir, baseName, outputFilePath, shellCmd, cli, res;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                lib = require('../../../../../methods/lib')();
+                args.inputs = lib.loadDefaultValues(args.inputs, details);
+
+                outputCodec = String(args.inputs.outputCodec || 'eac3').trim();
+                streams = (args.inputFileObj.ffProbeData && args.inputFileObj.ffProbeData.streams) || [];
+
+                opusAudioIndices = [];
+                audioIdx = 0;
+                streams.forEach(function (s) {
+                    if (s.codec_type === 'audio') {
+                        if (s.codec_name === 'opus') {
+                            opusAudioIndices.push(audioIdx);
+                        }
+                        audioIdx++;
+                    }
+                });
+
+                if (opusAudioIndices.length === 0) {
+                    args.jobLog('No OPUS audio streams found, skipping conversion');
+                    return [2, {
+                        outputFileObj: args.inputFileObj,
+                        outputNumber: 2,
+                        variables: args.variables,
+                    }];
+                }
+
+                args.jobLog('Found OPUS audio at audio stream index(es): ' + opusAudioIndices.join(', '));
+
+                codecOverrides = opusAudioIndices.map(function (i) {
+                    return '-c:a:' + i + ' ' + outputCodec;
+                }).join(' ');
+
+                path = require('path');
+                ext = path.extname(args.inputFileObj._id).slice(1) || 'mkv';
+                pluginWorkDir = (0, fileUtils_1.getPluginWorkDir)(args);
+                baseName = (0, fileUtils_1.getFileName)(args.inputFileObj._id);
+                outputFilePath = pluginWorkDir + '/' + baseName + '_opus_converted.' + ext;
+
+                shellCmd = 'ffmpeg -hide_banner -y'
+                    + ' -i "' + args.inputFileObj._id + '"'
+                    + ' -c copy'
+                    + ' ' + codecOverrides
+                    + ' "' + outputFilePath + '"';
+
+                args.jobLog('Running: ' + shellCmd);
+
+                cli = new cliUtils_1.CLI({
+                    cli: '/bin/sh',
+                    spawnArgs: ['-c', shellCmd],
+                    spawnOpts: {},
+                    jobLog: args.jobLog,
+                    outputFilePath: outputFilePath,
+                    inputFileObj: args.inputFileObj,
+                    logFullCliOutput: args.logFullCliOutput,
+                    updateWorker: args.updateWorker,
+                });
+
+                return [4, cli.runCli()];
+            case 1:
+                res = _a.sent();
+                if (res.cliExitCode !== 0) {
+                    args.jobLog('OPUS conversion failed');
+                    throw new Error('ffmpeg OPUS conversion failed');
+                }
+
+                args.logOutcome('tSuc');
+                return [2, {
+                    outputFileObj: { _id: outputFilePath },
+                    outputNumber: 1,
+                    variables: args.variables,
+                }];
+        }
+    });
+}); };
+exports.plugin = plugin;
